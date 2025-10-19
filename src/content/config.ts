6a97f5f2e606925789fa3a5e5e1fb032f7dec0fc
rafael-a-monteiro-math <rafael.a.monteiro.math@gmail.com>
2025-10-19 19:28:25 -0300

    website

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const projectsCollection = defineCollection({
+  schema: z.object({
+    inProgress: z.boolean(),
+    title: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    link: z.string(),
+    img_alt: z.string().optional(),
+    img_src: z.string().optional() // ✅ plain string now
+  }),
+});
+
+export const collections = {
+  projects: projectsCollection,
+};
---
+++ src/content/projects/project-1.md
@@ -0,0 +1,9 @@
+---
+inProgress: false
+title: Rising Sun Envelope Method
+description: 'The Rising Sun Envelope Method: an automatic and accurate peak location technique for XANES measurements'
+img_alt: project image alt text
+link: https://github.com/rafael-a-monteiro-math/Rising_Sun_Envelope_Method
+tags: ['Xanes', 'Peak detection', 'Noise reduction']
+img_src: assets/images/back_hanko_business_card_color3.png  # ✅ public path
+---
